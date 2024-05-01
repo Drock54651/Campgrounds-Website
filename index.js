93e2c9a73b09cbc5513d3eab8be23cdd9d85f902
Derick Sayavong <dsayavong54651@gmail.com>
2024-05-01 14:33:41 -0600

    edit and delte campgrounds

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const mongoose = require('mongoose')
+const methodOverride = require('method-override')
 const Campground = require('./models/campground')
 const campground = require('./models/campground')
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
+
+
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Connection error:"))
@@ -16,6 +19,7 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('_method'))
 
 //* Home Page and show all campgrounds
 app.get('/', async (req, res) =>{
@@ -47,7 +51,26 @@ app.get('/campgrounds/:id', async (req, res) =>{
 })
 
 
+//* Edit campgrounds
+app.get('/campgrounds/:id/edit',  async (req, res) =>{
+    const id = req.params.id
+    const campground = await Campground.findById(id)
+    res.render('campgrounds/edit', {campground})
+})
 
+app.put('/campgrounds/:id', async (req, res) =>{
+    const id = req.params.id
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
+    res.redirect(`/campgrounds/${id}`)
+})
+
+
+//* Delete campground
+app.delete('/campgrounds/:id', async (req, res) =>{
+    const id = req.params.id
+    const campground = await Campground.findByIdAndDelete(id)
+    res.redirect('/campgrounds')
+})
 
 app.listen(8000, () =>{
     console.log('LISTENING ON PORT 8000')
